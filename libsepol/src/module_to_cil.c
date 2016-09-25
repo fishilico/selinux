2c7886f8ffcf33f8b0ff0178ff31dee4c39a5195
Nicolas Iooss <nicolas.iooss@m4x.org>
2016-09-25 16:58:44 +0200

    Ignore -Wformat-nonliteral with pragmas

--- libsepol/src/module_to_cil.c
+++ libsepol/src/module_to_cil.c
@@ -1337,7 +1337,10 @@ static int cond_expr_to_cil(int indent, struct policydb *pdb, struct cond_expr *
 			// although we always supply val2 and there isn't always a 2nd
 			// value, it should only be used when there are actually two values
 			// in the format strings
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-nonliteral"
 			rlen = snprintf(new_val, len, fmt_str, op, val1, val2);
+#pragma GCC diagnostic pop
 			if (rlen < 0 || rlen >= len) {
 				log_err("Failed to generate conditional expression");
 				rc = -1;
@@ -1878,7 +1881,10 @@ static int constraint_expr_to_string(struct policydb *pdb, struct constraint_exp
 			// although we always supply val2 and there isn't always a 2nd
 			// value, it should only be used when there are actually two values
 			// in the format strings
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wformat-nonliteral"
 			rlen = snprintf(new_val, len, fmt_str, op, val1, val2);
+#pragma GCC diagnostic pop
 			if (rlen < 0 || rlen >= len) {
 				log_err("Failed to generate constraint expression");
 				rc = -1;
